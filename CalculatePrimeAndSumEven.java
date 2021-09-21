245c2f6edcb9a796d87cf5845116d6b4b7731492
Tanbir irfan <700038893@student.curtin.edu.my>
2021-09-21 19:08:32 +0600

    Modified Operation File

--- CalculatePrimeAndSumEven.java
+++ CalculatePrimeAndSumEven.java
@@ -1,21 +1,25 @@
-public CalculatePrimeAndSumEven{
-public int sumEvenNumbers(int[] elements)
-{
-	int total = 0;
-	try{
-	for(int i = 0; i <  elements.length; i++){
-		if(elements[i]%2 == 0){
-	total+=elements[i];
-}
+public class CalculatePrimeAndSumEven{
+	public static void main(String[] args) {
+		int[] elements = {-1, -2, -3, 0};
+		System.out.println(sumEvenNumbers(elements));
 	}
-}
-catch(NullPointerException e){
-System.out.println(e.getMessage())
-}
+	public static int sumEvenNumbers(int[] elements)
+	{
+		int total = 0;
+		try{
+			for(int i = 0; i <  elements.length; i++){
+				if(elements[i]%2 == 0){
+					total+=elements[i];
+				}
+			}
+		}
+		catch(NullPointerException e){
+			return -1;
+		}
 
-if((total == 0  && elements.length==0)   ||  elements.length == 0 || elements == null){
-	return -1;
-}
-return total;
-}
+		if((total == 0  && elements.length==0) || elements.length == 0){
+			return -1;
+		}
+		return total;
+	}
 }
